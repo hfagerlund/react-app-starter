9486f3640309bfba9c86ac58afe9518648f81513
Heini Fagerlund <heini@bytesofdesign.com>
2023-06-29 00:21:20 -0400

    Replace render with createRoot
    * Eliminates the following warning (from the browser console at http://localhost:8080/build/home.html):
    Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot react-dom.development.js:86:30
    * This uses the new root API, which creates a root running in React 18 (adds all of this version's improvements, and allows use of concurrent features).

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { App } from './components/App.jsx';
 
-render(
-  <App />,
-  document.getElementById('app')
-);
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(<App />);
